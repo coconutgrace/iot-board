7a1fbe9094d6312d0d5c6dd35ef29a3109591676
Tobias Kaupat <code@niondir.de>
2016-08-29 17:47:25 +0200

    Allow to load css as dependency

--- src/util/scriptLoader.ts
+++ src/util/scriptLoader.ts
@@ -1,12 +1,19 @@
 import loadjs = require('loadjs');
-
+import * as _ from 'lodash'
 
 // This is a class because we can not mock it on module level.
 export default class ScriptLoader {
     static loadScript(paths: Array<string>): Promise<void> {
+        const jsPaths = _.filter(paths, (p) => _.endsWith(p, ".js"))
+        const cssPaths = _.filter(paths, (p) => _.endsWith(p, ".css"))
+
+        _.forEach(cssPaths, (path) => {
+            $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', path));
+        })
+
         return new Promise<void>((resolve, reject) => {
             try {
-                loadjs(paths, {
+                loadjs(jsPaths, {
                     success: () => {
                         resolve();
                     },
