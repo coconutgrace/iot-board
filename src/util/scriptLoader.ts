dbf81affc95f81965e5462557b1c6ab0cfd5c71e
Tobias Kaupat <code@niondir.de>
2016-09-01 11:38:39 +0200

    Update npm dependencies and fix css loading

--- src/util/scriptLoader.ts
+++ src/util/scriptLoader.ts
@@ -1,19 +1,11 @@
 import loadjs = require('loadjs');
-import * as _ from 'lodash'
 
 // This is a class because we can not mock it on module level.
 export default class ScriptLoader {
     static loadScript(paths: Array<string>): Promise<void> {
-        const jsPaths = _.filter(paths, (p) => _.endsWith(p, ".js"))
-        const cssPaths = _.filter(paths, (p) => _.endsWith(p, ".css"))
-
-        _.forEach(cssPaths, (path) => {
-            $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', path));
-        })
-
         return new Promise<void>((resolve, reject) => {
             try {
-                loadjs(jsPaths, {
+                loadjs(paths, {
                     success: () => {
                         resolve();
                     },
